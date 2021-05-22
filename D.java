312a2e3924eadc6d0f2267b17537c79731baeb96
metisunita <sunitameti25@gmail.com>
2021-05-22 05:51:46 -0700

    Adds Alphbetprograms.java file

---
+++ D.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class D{
+public static void main(String[] args){
+int n=5;
+
+
+for(int i=0;i<=n;i++){
+	
+for(int j=0;j<=n;j++){
+if( i == 1 || i == 5 || j == 1 || j == 5)
+	
+System.out.print("*");
+	
+else
+System.out.print(" ");
+
+}
+System.out.println();
+}
+}
+}
